42f694278eb4de6b221559a1c791dff72d8d1a50
rodrigo viana <151778963+Dilunnn@users.noreply.github.com>
2025-05-15 17:25:28 -0300

    Criação da base do navbar

---
+++ Projeto-final/src/Components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='row'>
+      <div className="col">
+        {/* Logo do site */}
+      </div>
+      <div className="col">
+        {/* Barra de pesquisa */}
+      </div>
+      <div className="col">
+        {/* Cadraste-se */}
+      </div>
+      <div className="col">
+        {/* Entrar */}
+      </div>
+      <div className="col">
+        {/* Carrinho */}
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
