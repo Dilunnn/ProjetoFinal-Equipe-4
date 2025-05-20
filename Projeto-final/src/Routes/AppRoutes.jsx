14d3f26f556d91451e568c51af254d143a4bbd5b
rodrigo viana <151778963+Dilunnn@users.noreply.github.com>
2025-05-20 17:30:41 -0300

    começo da criação do style do Navbar

--- Projeto-final/src/Routes/AppRoutes.jsx
+++ Projeto-final/src/Routes/AppRoutes.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
-import Cadrasto from '../pages/Cadrasto';
+import Cadastro from '../pages/Cadastro';
 import Entrar from '../pages/Entrar';
 import Carrinho from '../pages/Carrinho'
 
@@ -8,7 +8,7 @@ export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home/>} />
-      <Route path='/cadrasto' element={<Cadrasto/>}/>
+      <Route path='/cadastro' element={<Cadastro/>}/>
       <Route path='/entrar' element={<Entrar/>}/>
       <Route path='/carrinho' element={<Carrinho/>}/>
       
