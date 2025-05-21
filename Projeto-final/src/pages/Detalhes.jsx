18054ca202449fc2d274997e1bf2b5736651f11e
Luis-C-Dev <luiscarlosantonio16@gmail.com>
2025-05-21 19:57:31 -0300

    adição do "produtos relacionados"
    adicionado o css da pagina detalhes.
    adicionado o "produtos relacionados"
    adicionado as imagens ao public

---
+++ Projeto-final/src/pages/Detalhes.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Link} from 'react-router-dom'
+import style from './Detalhes.module.css'
+
+
+const Detalhes = () => {
+  return (
+    <div>
+      {/* produto e detalhes do produto */}
+
+    <div className={style.produtos_titulo}>
+      {/*produtos relacionados*/}
+      <p className={style.produtosR}>produtos relacionados</p>
+      <Link to='/produtos' > ver todos  
+      <span>
+        <svg xmlns="http://www.w3.org/2000/svg" 
+        width="16"
+        height="16" 
+        fill="currentColor"
+        className="bi bi-arrow-right" viewBox="0 0 16 16">
+            <path fillRule="evenodd"
+              d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
+          </svg></span></Link>
+
+
+    </div>
+    </div>
+  )
+}
+
+export default Detalhes
