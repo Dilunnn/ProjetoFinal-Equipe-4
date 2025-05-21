9149a400c8aad04bcb2c557c42bfac199f694238
Geração Tech <geracaotech@digitalcollege.internal>
2025-05-21 08:40:27 -0300

    adicionando rotas da página

--- Projeto-final/src/App.jsx
+++ Projeto-final/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     <div className='container'>
         <BrowserRouter>
             <Navbar/>
-            
+            <AppRoutes/>
             <Footer/>
         </BrowserRouter>
     </div>
--- Projeto-final/src/pages/Entrar.jsx
+++ Projeto-final/src/pages/Entrar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Entrar = () => {
   return (
     <div>
-      <h1></h1>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. In fugit voluptatibus unde maiores alias tempore aspernatur, vel, eligendi a deserunt, temporibus voluptate. Amet labore praesentium voluptate enim sint doloribus repellat?</h1>
     </div>
   )
 }
