4582f825f43a170e253844d42db6bb2e05776631
Luis-C-Dev <luiscarlosantonio16@gmail.com>
2025-05-22 09:58:44 -0300

    adicionei a lista de sujestões na pagina 'detalhes'

--- Projeto-final/src/pages/Detalhes.jsx
+++ Projeto-final/src/pages/Detalhes.jsx
@@ -7,6 +7,7 @@ import style from './Detalhes.module.css'
 const Detalhes = () => {
   return (
     <div>
+      <main>
       {/* produto e detalhes do produto */}
 
     <div className={style.produtos_titulo}>
@@ -22,10 +23,54 @@ const Detalhes = () => {
             <path fillRule="evenodd"
               d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
           </svg></span></Link>
+    </div>
+
+      {/* sujestões */}
+    <div className={style.lista}>
+      <div className={"card"+ style.carta} style={{width: '18rem'}}>
+        <img src="/K-Siss V8-Masculino.png" className="card-img-top" alt="..."/>
+        <div className="card-body">
+          <p style={{color: 'gray'}}>tenis</p>
+          <p className="card-text" style={{color: 'gray'}}>K-Siss V8 - masculino</p>
+          <span style={{textDecoration: 'line-through', color: 'gray'}}>$200</span> <span>$100</span>
+        </div>
+      </div>
+
+      <div className={"card"+ style.carta} style={{width: '18rem'}}>
+        <img src="/K-Siss V8-Masculino.png" className="card-img-top" alt="..."/>
+        <div className="card-body">
+          <p style={{color: 'gray'}}>tenis</p>
+          <p className="card-text" style={{color: 'gray'}}>K-Siss V8 - masculino</p>
+          <span style={{textDecoration: 'line-through', color: 'gray'}}>$200</span> <span>$100</span>
+        </div>
+      </div>
+      
+      <div className={"card"+ style.carta} style={{width: '18rem'}}>
+        <img src="/K-Siss V8-Masculino.png" className="card-img-top" alt="..."/>
+        <div className="card-body">
+          <p style={{color: 'gray'}}>tenis</p>
+          <p className="card-text" style={{color: 'gray'}}>K-Siss V8 - masculino</p>
+          <span style={{textDecoration: 'line-through', color: 'gray'}}>$200</span> <span>$100</span>
+        </div>
+      </div>
+
+      <div className={"card"+ style.carta} style={{width: '18rem'}}>
+        <img src="/K-Siss V8-Masculino.png" className="card-img-top" alt="..."/>
+        <div className="card-body">
+          <p style={{color: 'gray'}}>tenis</p>
+          <p className="card-text" style={{color: 'gray'}}>K-Siss V8 - masculino</p>
+          <span style={{textDecoration: 'line-through', color: 'gray'}}>$200</span> <span>$100</span>
+        </div>
+      </div>
 
 
+        {/* são 4 */}
+      </div>
+
+</main>
     </div>
-    </div>
+
+    
   )
 }
 
