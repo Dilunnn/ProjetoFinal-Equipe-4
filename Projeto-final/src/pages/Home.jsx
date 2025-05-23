6464e15defe708b086c31efcf2a0046b0a33648c
Luizz908 <luizf.bezerra9@gmail.com>
2025-05-23 10:22:05 -0300

    Atualização do CSS

--- Projeto-final/src/pages/Home.jsx
+++ Projeto-final/src/pages/Home.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import styles from "./Home.module.css";
+
+
+
+
+
 
 const Home = () => {
   return (
     <>
-
-      <section className="hero">
-        <div className="hero-text">
-          <span className="destaque">Melhores ofertas personalizadas</span>
+      <section className={styles.hero}>
+        <div className={styles.heroText}>
+          <span className={styles.destaque}>
+            Melhores ofertas personalizadas
+          </span>
           <h1>
             Queima de estoque Nike <span>🔥</span>
           </h1>
@@ -14,13 +21,10 @@ const Home = () => {
             Consequat culpa exercitation mollit nisi excepteur do do tempor
             laboris eiusmod irure consectetur.
           </p>
-          <button className="btn-hero">Ver Ofertas</button>
+          <button className={styles.btnHero}>Ver Ofertas</button>
         </div>
-        <div className="hero-img">
-          <img
-            src="Imagens/White-Sneakers-PNG-Clipart 1.svg"
-            alt="Tênis Nike"
-          />
+        <div className={styles.heroImg}>
+          <img src="../assets/ImagesHome/ddd 1.svg" alt="" />
         </div>
       </section>
 
@@ -29,166 +33,161 @@ const Home = () => {
         <div className="row g-4">
           {/* Card 1 */}
           <div className="col-md-4">
-            <div className="card card-custom h-100">
-              <span className="badge-desconto">30% OFF</span>
+            <div className={styles.card.cardCustom}>
+              <span className={styles.badgeDesconto}>30% OFF</span>
               <h5 className="mt-3">
                 Novo drop <br /> <strong>Supreme</strong>
               </h5>
-              <button className="btn btn-comprar">Comprar</button>
-              <img
-                src="Imagens/Mask group.svg"
-                alt="Supreme"
-                className="card-img"
-              />
+              <button className={styles.btnComprar}>Comprar</button>
+              <img src="Imagens/Mask group.svg" alt="Supreme" />
             </div>
           </div>
 
           {/* Card 2 */}
           <div className="col-md-4">
-            <div className="card card-custom h-100">
-              <span className="badge-desconto">30% OFF</span>
+            <div className={styles.card.cardCustom}>
+              <span className={styles.badgeDesconto}>30% OFF</span>
               <h5 className="mt-3">
                 Coleção <br /> <strong>Adidas</strong>
               </h5>
-              <button className="btn btn-comprar">Comprar</button>
-              <img src="Imagens/ddd 1.svg" alt="Adidas" className="card-img" />
+              <button className={styles.btnComprar}>Comprar</button>
+              <img src="Imagens/ddd 1.svg" alt="Adidas" />
             </div>
           </div>
 
           {/* Card 3 */}
           <div className="col-md-4">
-            <div className="card card-custom h-100">
-              <span className="badge-desconto">30% OFF</span>
+            <div className={styles.card.cardCustom}>
+              <span className={styles.badgeDesconto}>30% OFF</span>
               <h5 className="mt-3">
                 Novo <br /> <strong>Beats Bass</strong>
               </h5>
-              <button className="btn btn-comprar">Comprar</button>
+              <button className={styles.btnComprar}>Comprar</button>
               <img
                 src="Imagens/d9db11953a2d185d37246bb1f500c957 1.svg"
                 alt="Beats"
-                className="card-img"
               />
             </div>
           </div>
         </div>
       </div>
 
-      <h3 className="destaque">Coleções em Destaque</h3>
+      <h3 className={styles.destaque}>Coleções em Destaque</h3>
 
-      <div className="icones">
+      <div className={styles.icones}>
         <img src="Imagens/Frame 10.png" alt="Camisa" />
         <img src="Imagens/Frame 11.png" alt="calca" />
         <img src="Imagens/Frame 13.png" alt="tenis" />
         <img src="Imagens/Frame 14.png" alt="fone" />
       </div>
 
-      <section className="produtos">
+      <section className={styles.produtos}>
         <h2>Produtos em Alta</h2>
-        <div className="cards">
-          <div className="card">
+        <div className={styles.cards}>
+          <div className={styles.card}>
             <img
               src="Imagens/Layer 1aa 2.png"
               alt="Tênis K-Swiss V8 Masculino"
             />
-            <p className="categoria">Tênis</p>
-            <p className="nome">K-Swiss V8 - Masculino</p>
-            <div className="precos">
-              <p className="preco-antigo">R$200</p>
-              <p className="preco-novo">R$100</p>
+            <p className={styles.categoria}>Tênis</p>
+            <p className={styles.nome}>K-Swiss V8 - Masculino</p>
+            <div className={styles.precos}>
+              <p className={styles.precoAntigo}>R$200</p>
+              <p className={styles.precoNovo}>R$100</p>
             </div>
           </div>
-          <div className="card">
+          <div className={styles.card}>
             <img
               src="Imagens/Layer 1aa 2.png"
               alt="Tênis K-Swiss V8 Masculino"
             />
-            <p className="categoria">Tênis</p>
-            <p className="nome">K-Swiss V8 - Masculino</p>
-            <div className="precos">
-              <p className="preco-antigo">R$200</p>
-              <p className="preco-novo">R$100</p>
+            <p className={styles.categoria}>Tênis</p>
+            <p className={styles.nome}>K-Swiss V8 - Masculino</p>
+            <div className={styles.precos}>
+              <p className={styles.precoAntigo}>R$200</p>
+              <p className={styles.precoNovo}>R$100</p>
             </div>
           </div>
-          <div className="card">
+          <div className={styles.card}>
             <img
               src="Imagens/Layer 1aa 2.png"
               alt="Tênis K-Swiss V8 Masculino"
             />
-            <p className="categoria">Tênis</p>
-            <p className="nome">K-Swiss V8 - Masculino</p>
-            <div className="precos">
-              <p className="preco-antigo">R$200</p>
-              <p className="preco-novo">R$100</p>
+            <p className={styles.categoria}>Tênis</p>
+            <p className={styles.nome}>K-Swiss V8 - Masculino</p>
+            <div className={styles.precos}>
+              <p className={styles.precoAntigo}>R$200</p>
+              <p className={styles.precoNovo}>R$100</p>
             </div>
           </div>
-          <div className="card">
+          <div className={styles.card}>
             <img
               src="Imagens/Layer 1aa 2.png"
               alt="Tênis K-Swiss V8 Masculino"
             />
-            <p className="categoria">Tênis</p>
-            <p className="nome">K-Swiss V8 - Masculino</p>
-            <div className="precos">
-              <p className="preco-antigo">R$200</p>
-              <p className="preco-novo">R$100</p>
+            <p className={styles.categoria}>Tênis</p>
+            <p className={styles.nome}>K-Swiss V8 - Masculino</p>
+            <div className={styles.precos}>
+              <p className={styles.precoAntigo}>R$200</p>
+              <p className={styles.precoNovo}>R$100</p>
             </div>
           </div>
         </div>
         <br />
-        <div className="cards">
-          <div className="card">
+        <div className={styles.cards}>
+          <div className={styles.card}>
             <img
               src="Imagens/Layer 1aa 2.png"
               alt="Tênis K-Swiss V8 Masculino"
             />
-            <p className="categoria">Tênis</p>
-            <p className="nome">K-Swiss V8 - Masculino</p>
-            <div className="precos">
-              <p className="preco-antigo">R$200</p>
-              <p className="preco-novo">R$100</p>
+            <p className={styles.categoria}>Tênis</p>
+            <p className={styles.nome}>K-Swiss V8 - Masculino</p>
+            <div className={styles.precos}>
+              <p className={styles.precoAntigo}>R$200</p>
+              <p className={styles.precoNovo}>R$100</p>
             </div>
           </div>
-          <div className="card">
+          <div className={styles.card}>
             <img
               src="Imagens/Layer 1aa 2.png"
               alt="Tênis K-Swiss V8 Masculino"
             />
-            <p className="categoria">Tênis</p>
-            <p className="nome">K-Swiss V8 - Masculino</p>
-            <div className="precos">
-              <p className="preco-antigo">R$200</p>
-              <p className="preco-novo">R$100</p>
+            <p className={styles.categoria}>Tênis</p>
+            <p className={styles.nome}>K-Swiss V8 - Masculino</p>
+            <div className={styles.precos}>
+              <p className={styles.precoAntigo}>R$200</p>
+              <p className={styles.precoNovo}>R$100</p>
             </div>
           </div>
-          <div className="card">
+          <div className={styles.card}>
             <img
               src="Imagens/Layer 1aa 2.png"
               alt="Tênis K-Swiss V8 Masculino"
             />
-            <p className="categoria">Tênis</p>
-            <p className="nome">K-Swiss V8 - Masculino</p>
-            <div className="precos">
-              <p className="preco-antigo">R$200</p>
-              <p className="preco-novo">R$100</p>
+            <p className={styles.categoria}>Tênis</p>
+            <p className={styles.nome}>K-Swiss V8 - Masculino</p>
+            <div className={styles.precos}>
+              <p className={styles.precoAntigo}>R$200</p>
+              <p className={styles.precoNovo}>R$100</p>
             </div>
           </div>
-          <div className="card">
+          <div className={styles.card}>
             <img
               src="Imagens/Layer 1aa 2.png"
               alt="Tênis K-Swiss V8 Masculino"
             />
-            <p className="categoria">Tênis</p>
-            <p className="nome">K-Swiss V8 - Masculino</p>
-            <div className="precos">
-              <p className="preco-antigo">R$200</p>
-              <p className="preco-novo">R$100</p>
+            <p className={styles.categoria}>Tênis</p>
+            <p className={styles.nome}>K-Swiss V8 - Masculino</p>
+            <div className={styles.precos}>
+              <p className={styles.precoAntigo}>R$200</p>
+              <p className={styles.precoNovo}>R$100</p>
             </div>
           </div>
         </div>
       </section>
 
-      <section className="oferta-especial">
-        <div className="container">
+      <section className={styles.ofertaEspecial}>
+        <div className={styles.container}>
           <div className="row align-items-center">
             <div className="col-md-6 text-center">
               <img
@@ -198,7 +197,7 @@ const Home = () => {
               />
             </div>
             <div className="col-md-6">
-              <div className="subtitulo">Oferta especial</div>
+              <div className={styles.subtitulo}>Oferta especial</div>
               <h2 className="fw-bold text-black">
                 Air Jordan edição de colecionador
               </h2>
@@ -208,7 +207,7 @@ const Home = () => {
                 perferendis deserunt distinctio hic assumenda esse, voluptatum
                 odit error quae ipsam facere libero repellat?
               </p>
-              <a href="#" className="btn btn-oferta">
+              <a href="#" className={styles.btnOferta}>
                 Ver Oferta
               </a>
             </div>
