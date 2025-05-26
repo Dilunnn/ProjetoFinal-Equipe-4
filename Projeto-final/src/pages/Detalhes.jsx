0addacd22efee421d3c2f51745d94681e8ad2fde
rodrigo viana <151778963+Dilunnn@users.noreply.github.com>
2025-05-25 23:42:30 -0300

    tirando imports desnecessários
    tirei o import do react já que ele nunca é chamado, e o import do bootstrap algo que já acontece no main

--- Projeto-final/src/pages/Detalhes.jsx
+++ Projeto-final/src/pages/Detalhes.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import {Link} from 'react-router-dom'
 import style from './Detalhes.module.css'
 import { useState } from 'react';
